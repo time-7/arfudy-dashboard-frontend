60574909a79f42001ed5b37b3676e0149c3a79ac
gfmanica <gabrielfelipemanica@gmail.com>
2023-09-19 00:36:34 -0300

    CU-861nbr2j7 - Alterar pacote de componentes para a MUI - Alterado projeto para usar a biblioteca da MUI.

---
+++ src/components/navbar.tsx
@@ -0,0 +1,11 @@
+import { Paper } from '@mui/material';
+
+export default function Navbar() {
+  return (
+    <Paper
+      variant="outlined"
+      elevation={4}
+      sx={{ backgroundColor: '#0c55a8', width: '9rem', borderRadius: 3 }}
+    ></Paper>
+  );
+}
