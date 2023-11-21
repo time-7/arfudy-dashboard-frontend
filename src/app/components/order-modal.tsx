8b7557c02232ceeb08843a6dc197834d683328c3
Gabriel Felipe Manica <gabriel.manica@ciss.com.br>
2023-11-21 13:22:53 -0300

    CU-866b0kbhk - Exibir pedidos na tela inicial - Criado o modal pra exibição dos dados do pedido

--- src/app/components/order-modal.tsx
+++ src/app/components/order-modal.tsx
@@ -1,9 +1,45 @@
+import { Dispatch, SetStateAction } from 'react';
+
 import { TOrder } from '@/types';
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from '@mui/material';
 
 type TOrderModal = {
   order: TOrder;
+  open: boolean;
+  setOpen: Dispatch<SetStateAction<boolean>>;
 };
 
-export default function OrderModal({ order }: TOrderModal) {
-  return <div>order-modal</div>;
+export default function OrderModal({ order, open, setOpen }: TOrderModal) {
+  const onClose = () => setOpen(false);
+
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      PaperProps={{
+        sx: {
+          borderRadius: 4,
+          backgroundColor: 'secondary.main',
+          border: '2px solid #000',
+        },
+      }}
+    >
+      <DialogTitle>{order.product.name}</DialogTitle>
+      <DialogContent>
+        <DialogContentText>fsdfddfs</DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button autoFocus onClick={onClose}>
+          Fechar
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 }
--- src/components/cards/order-card.tsx
+++ src/components/cards/order-card.tsx
@@ -5,12 +5,16 @@ import { useDrag } from 'react-dnd';
 
 import { TOrder } from '@/types';
 import { Box, Paper, Typography } from '@mui/material';
+import OrderModal from '@/app/components/order-modal';
+import { useState } from 'react';
 
 type TOrderCard = {
   order: TOrder;
 };
 
 export default function OrderCard({ order }: TOrderCard) {
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
   const drag = useDrag(() => ({
     item: order,
     type: 'TOrder',
@@ -18,54 +22,60 @@ export default function OrderCard({ order }: TOrderCard) {
   }))[1];
 
   return (
-    <Paper
-      ref={drag}
-      elevation={2}
-      sx={{
-        backgroundColor: 'secondary.main',
-        height: 200,
-        width: 290,
-        boxShadow: '0px 7px 0px 0px #000',
-        borderRadius: '30px',
-        border: '2px solid #000',
-      }}
-    >
-      <Box
+    <>
+      <OrderModal open={isModalOpen} setOpen={setIsModalOpen} order={order} />
+
+      <Paper
+        ref={drag}
+        elevation={2}
+        onClick={() => setIsModalOpen(true)}
         sx={{
-          width: 286,
-          height: 100,
-          position: 'relative',
+          backgroundColor: 'secondary.main',
+          height: 200,
+          width: 290,
+          boxShadow: '0px 7px 0px 0px #000',
+          borderRadius: '30px',
+          border: '2px solid #000',
+          cursor: 'pointer',
         }}
       >
-        <Image
-          src="https://files.edgestore.dev/k1h85kjlhc106fdq/publicFiles/_public/4c5a4b46-affc-4b28-b5b3-13a3dcc35361.png"
-          alt="kk"
-          unoptimized
-          fill
-          style={{
-            borderTopLeftRadius: '30px',
-            borderTopRightRadius: '30px',
+        <Box
+          sx={{
+            width: 286,
+            height: 100,
+            position: 'relative',
           }}
-        />
-      </Box>
-      <Box
-        sx={{
-          borderTop: '2px solid #000',
-          display: 'flex',
-          height: '100px',
-          flexDirection: 'column',
-          paddingLeft: 1.5,
-          paddingRight: 1.5,
-          justifyContent: 'center',
-        }}
-      >
-        <Typography sx={{ color: '#fff', fontSize: '20px', fontWeight: 600 }}>
-          {order.product.name}
-        </Typography>
-        <Typography sx={{ fontSize: '16px', color: '#fff' }}>
-          Mesa 01
-        </Typography>
-      </Box>
-    </Paper>
+        >
+          <Image
+            src="https://files.edgestore.dev/k1h85kjlhc106fdq/publicFiles/_public/4c5a4b46-affc-4b28-b5b3-13a3dcc35361.png"
+            alt="kk"
+            unoptimized
+            fill
+            style={{
+              borderTopLeftRadius: '30px',
+              borderTopRightRadius: '30px',
+            }}
+          />
+        </Box>
+        <Box
+          sx={{
+            borderTop: '2px solid #000',
+            display: 'flex',
+            height: '100px',
+            flexDirection: 'column',
+            paddingLeft: 1.5,
+            paddingRight: 1.5,
+            justifyContent: 'center',
+          }}
+        >
+          <Typography sx={{ color: '#fff', fontSize: '20px', fontWeight: 600 }}>
+            {order.product.name}
+          </Typography>
+          <Typography sx={{ fontSize: '16px', color: '#fff' }}>
+            Mesa 01
+          </Typography>
+        </Box>
+      </Paper>
+    </>
   );
 }
