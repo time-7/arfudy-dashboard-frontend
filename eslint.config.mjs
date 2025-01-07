5e92ba39b867c81a2e62bdf4da043ecb476cd037
gabrielmanica <gabriel.manica@ciss.com.br>
2025-01-07 20:21:09 -0300

    refactor: simplifica configuração do ESLint e remove regras obsoletas

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,60 +1,25 @@
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-import { fixupPluginRules } from '@eslint/compat';
-import { FlatCompat } from '@eslint/eslintrc';
-import js from '@eslint/js';
-import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
-import react from 'eslint-plugin-react';
-import reactHooks from 'eslint-plugin-react-hooks';
-import globals from 'globals';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [
-    ...compat.extends(
+// eslint.config.mjs
+export default {
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+        es2021: true
+    },
+    extends: [
         'eslint:recommended',
-        'plugin:@typescript-eslint/recommended',
         'plugin:react/recommended',
-        'plugin:prettier/recommended',
-        'plugin:@next/next/recommended',
-        'prettier'
-    ),
-    {
-        plugins: {
-            '@typescript-eslint': typescriptEslint,
-            react,
-            'react-hooks': fixupPluginRules(reactHooks)
-        },
-
-        languageOptions: {
-            globals: {
-                ...globals.browser
-            },
-
-            parser: tsParser,
-            ecmaVersion: 'latest',
-            sourceType: 'module'
-        },
-
-        settings: {
-            react: {
-                version: 'detect'
-            }
-        },
-
-        rules: {
-            'react-hooks/rules-of-hooks': 'error',
-            'react-hooks/exhaustive-deps': 'warn',
-            'react/react-in-jsx-scope': 'off',
-            'prettier/prettier': 'error'
-        }
+        'plugin:react-hooks/recommended'
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: ['react', 'react-hooks'],
+    rules: {
+        'react/prop-types': 'off', // Desabilitar verificação de prop-types
+        'no-console': 'warn', // Aviso sobre uso de console
+        'react/jsx-uses-react': 'off', // Desabilitar regra do React (não é mais necessário no React 17+)
+        'react/react-in-jsx-scope': 'off' // Desabilitar regra do React (não é mais necessário no React 17+)
     }
-];
+};
